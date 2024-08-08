3d9ed8bc5407dd5a9742e8bca61d244e6a83be41
Juracy Junior <jfcjuniorr@gmail.com>
2024-08-08 16:13:07 -0300

    Environment in production.

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    urlBase: 'https://cdb-investment-api-ddd3e8h9heggfuhx.eastus-01.azurewebsites.net/api/cdb'
+}
--- app/src/environments/environments.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    urlBase: 'https://cdb-investment-api.juracyjunior.com/api/cdb'
-}
